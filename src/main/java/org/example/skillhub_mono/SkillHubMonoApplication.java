18929598393fa1b410b40f84354642660b572a1e
Nezhdanchik1 <myktybekov1234@gmail.com>
2026-02-12 15:50:21 +0500

    Initialized project

---
+++ src/main/java/org/example/skillhub_mono/SkillHubMonoApplication.java
@@ -0,0 +1,13 @@
+package org.example.skillhub_mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillHubMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillHubMonoApplication.class, args);
+    }
+
+}
